9761da77bb36561737fe30ea4ccb47cdbb8323ea
jhon125892 <johnsteven747474@gmail.com>
2025-08-15 09:03:29 -0500

    ejer1

---
+++ actividad_15agosto_2025/bin/dart1.dart
@@ -0,0 +1,64 @@
+// IMPORT
+import 'dart:io';
+//
+void main() {
+ // seleccion
+
+  print(" ");
+  print("Hola!, aqui podras calcular las propinas de su servicio de delivery!");
+  print("Porfavor, seleccione su servicio:");
+  print("1. Comida");
+  print("2. Farmacia");
+  print("3. Supermercado");
+
+print ("Servicio: ");
+  int seleccion = 0;
+  seleccion = int.parse(stdin.readLineSync()!);
+// --------------- //
+
+print("Porfavor, seleccione el servicio dado por el usuario:");
+  print("1. Excelente");
+  print("2. Bueno");
+  print("3. Regular");
+
+  int calificacionServicio = 0;
+  calificacionServicio = int.parse(stdin.readLineSync()!);
+
+
+  (print("Ingrese el valor del pedido: "));
+  int valorPedido = 0;
+  valorPedido = int.parse(stdin.readLineSync()!);
+
+  double propinaFinal = 0;
+
+
+  if (calificacionServicio == 1){
+    print("Excelente");
+    propinaFinal = valorPedido * 0.20;
+
+  } else
+  if (calificacionServicio == 2) {
+    print("Bueno");
+    propinaFinal = valorPedido * 0.15;
+  } else
+  if (calificacionServicio == 3){
+    print("Regular");
+    propinaFinal = valorPedido * 0.10;
+  }
+  
+
+
+    if (seleccion == 1){
+    print("Has seleccionado el servicio de Comida");
+    print("Tu propina sera de $propinaFinal");
+  } else 
+  if (seleccion == 2){
+    print("Has seleccionado el servicio de Farmacia");
+    print("Tu propina sera de $propinaFinal");
+  } else 
+  if (seleccion == 3){
+    print("Has seleccionado el servicio de Supermercado");
+    print("Tu propina sera de $propinaFinal");
+  }
+  
+}
